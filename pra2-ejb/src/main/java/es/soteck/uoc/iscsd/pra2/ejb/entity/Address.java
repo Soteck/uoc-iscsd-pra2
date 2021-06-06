ed31e5c32fa0fbb252f18ced29d926ce9cfeff62
Soteck <tukav73@gmail.com>
2021-06-06 13:40:55 +0200

    - Añadir lombok y refactor

--- pra2-ejb/src/main/java/es/soteck/uoc/iscsd/pra2/ejb/entity/Address.java
+++ pra2-ejb/src/main/java/es/soteck/uoc/iscsd/pra2/ejb/entity/Address.java
@@ -1,5 +1,9 @@
 package es.soteck.uoc.iscsd.pra2.ejb.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -7,78 +11,25 @@ import java.io.Serializable;
 
 
 @Entity
-@Table(name="uoccsd.address")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "address", schema = "uoccsd")
 public class Address implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
 
+
+	@Id
 	private int id;
 	private String street;
 	private String city;
 	private String state;
 	private String zip;
-	
-
-	public Address() {
-		super();
-	}
-
-	public Address(int id, String street, String city, String state, String zip) {
-		
-		this.id = id;
-		this.street = street;
-		this.city = city;
-		this.state = state;
-		this.zip = zip;
-	}
 
 
-	@Id
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getStreet() {
-		return street;
-	}
-	
-	public void setStreet(String street) {
-		this.street = street;
-	}
-	
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	
-	public String getZip() {
-		return zip;
-	}
-
-	public void setZip(String zip) {
-		this.zip = zip;
-	}
-	
 	@Override
 	public String toString() {
 
 		return "Address: " + getId() + " Street " + getStreet() + " City "
-				+ getCity()+ " State "+ getState()+ " Zip "+ getZip();
+				+ getCity() + " State " + getState() + " Zip " + getZip();
 	}
 }
--- pra2-ejb/src/main/java/es/soteck/uoc/iscsd/pra2/ejb/entity/Customer.java
+++ pra2-ejb/src/main/java/es/soteck/uoc/iscsd/pra2/ejb/entity/Customer.java
@@ -1,72 +1,31 @@
 package es.soteck.uoc.iscsd.pra2.ejb.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 import java.io.Serializable;
 
 @Entity
-@Table(name="uoccsd.customer")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "customer", schema = "uoccsd")
 public class Customer implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	private int id;
-	private String last_name;
-	private String first_name;
-	private Address address;
-	
-
-	public Customer() {
-		super();
-	}
-
-	public Customer(int id, String last_name, String first_name) {
-		
-		this.id = id;
-		this.last_name = last_name;
-		this.first_name = first_name;
-	}
 
 	@Id
+	private int id;
 
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	
 	@Column(name = "last_name")
-	public String getLastName() {
-		return last_name;
-	}
-	public  void setLastName(String last_name) {
-		this.last_name = last_name;
-	}
+	private String last_name;
 
 	@Column(name = "first_name")
-	public String getFirstName() {
-		return first_name;
-	}	
-	public void setFirstName(String first_name) {
-		this.first_name = first_name;
-	}
+	private String first_name;
 
-	// persistent relationships
 	@OneToOne
 	@JoinColumn(name = "home_address")
-	public Address getHomeAddress() {
-		return address;
-	}	
-	public void setHomeAddress(Address address) {
-		this.address = address;
-	}
-	
-	@Override
-	public String toString() {
+	private Address address;
 
-		return "Customer: " + getId() + " Name " + getLastName() + ", "
-				+ getFirstName();
-	}
+
 }
