b846b3795dc96c4a45ca08d651ab54ffd7d9dc76
Soteck <tukav73@gmail.com>
2021-06-10 00:07:41 +0200

    - Faltaba un @Local en el repository

--- pra2-ejb/src/main/java/es/soteck/uoc/iscsd/pra2/ejb/repository/CategoryRepositoryInterface.java
+++ pra2-ejb/src/main/java/es/soteck/uoc/iscsd/pra2/ejb/repository/CategoryRepositoryInterface.java
@@ -3,5 +3,8 @@ package es.soteck.uoc.iscsd.pra2.ejb.repository;
 import es.soteck.uoc.iscsd.pra2.ejb.base.BaseRepository;
 import es.soteck.uoc.iscsd.pra2.ejb.entity.Category;
 
+import javax.ejb.Local;
+
+@Local
 public interface CategoryRepositoryInterface extends BaseRepository<Category> {
 }
