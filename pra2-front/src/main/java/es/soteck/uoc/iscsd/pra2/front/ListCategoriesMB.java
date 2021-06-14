8e673ab8795aade1e1f4ae57711636ff7e09808e
Soteck <tukav73@gmail.com>
2021-06-14 23:03:06 +0200

    - Implementar listado y detall de categoria en frontal

---
+++ pra2-front/src/main/java/es/soteck/uoc/iscsd/pra2/front/ListCategoriesMB.java
@@ -0,0 +1,27 @@
+package es.soteck.uoc.iscsd.pra2.front;
+
+import es.soteck.uoc.iscsd.pra2.ejb.bean.CategoryLocal;
+import es.soteck.uoc.iscsd.pra2.ejb.pojo.CategoryVO;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.List;
+
+@SessionScoped
+@ManagedBean(name = "ListCategoriesMB")
+public class ListCategoriesMB {
+
+	@EJB
+	private CategoryLocal categoryLocal;
+
+
+	public List<CategoryVO> getCategories() {
+		return categoryLocal.list();
+	}
+
+	public String listCategories() {
+		return "listCategoryView";
+	}
+
+}
