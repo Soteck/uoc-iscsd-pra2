23a08b345c89a18b99eedcf6331a81c709f7a209
Soteck <tukav73@gmail.com>
2021-06-18 18:48:30 +0200

    - Primeros pasos para añadir las vistas de añadir y modificar categoria

---
+++ pra2-front/src/main/java/es/soteck/uoc/iscsd/pra2/front/AddCategoryMB.java
@@ -0,0 +1,23 @@
+package es.soteck.uoc.iscsd.pra2.front;
+
+import es.soteck.uoc.iscsd.pra2.ejb.bean.CategoryLocal;
+import es.soteck.uoc.iscsd.pra2.ejb.pojo.CategoryVO;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@ManagedBean(name = "AddCategoryMB")
+public class AddCategoryMB {
+
+	@EJB
+	private CategoryLocal categoryLocal;
+
+	private CategoryVO category = new CategoryVO();
+
+	public CategoryVO getCategory() {
+		return category;
+	}
+
+}
---
+++ pra2-front/src/main/java/es/soteck/uoc/iscsd/pra2/front/EditCategoryMB.java
@@ -0,0 +1,30 @@
+package es.soteck.uoc.iscsd.pra2.front;
+
+import es.soteck.uoc.iscsd.pra2.ejb.bean.CategoryLocal;
+import es.soteck.uoc.iscsd.pra2.ejb.pojo.CategoryVO;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@SessionScoped
+@ManagedBean(name = "EditCategoryMB")
+public class EditCategoryMB {
+
+	@EJB
+	private CategoryLocal categoryLocal;
+
+	protected String categoryName = null;
+
+	public CategoryVO getCategory(){
+		return categoryLocal.findByName(categoryName);
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+}
